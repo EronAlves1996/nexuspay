4e6ec9dc0db2c917ee8935a3c82eab96b95ee0b2
Eron Alves <eron.alves@rocketmail.com>
2026-05-07 14:49:55 -0300

    feat: create mapping from wallet

--- src/main/java/com/eronalves/nexuspay/wallet/Wallet.java
+++ src/main/java/com/eronalves/nexuspay/wallet/Wallet.java
@@ -5,11 +5,13 @@ import java.util.UUID;
 import org.hibernate.annotations.SoftDelete;
 import com.eronalves.nexuspay.infra.SensibleEntity;
 import com.eronalves.nexuspay.user.User;
+import com.eronalves.nexuspay.wallet.WalletController.CreateWalletDto;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.Valid;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,6 +20,14 @@ import lombok.Setter;
 @Setter
 @SoftDelete
 public class Wallet extends SensibleEntity {
+
+  public static Wallet from(@Valid CreateWalletDto dto) {
+    Wallet wallet = new Wallet();
+    wallet.setUserId(dto.userId());
+    wallet.setName(dto.name());
+    wallet.setMinLimit(BigDecimal.ZERO);
+    return wallet;
+  }
 
   @Column(nullable = false, length = 255)
   private String name;
@@ -30,5 +40,6 @@ public class Wallet extends SensibleEntity {
   private User user;
 
   private UUID userId;
+
 
 }

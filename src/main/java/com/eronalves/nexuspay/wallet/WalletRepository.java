0d8a7c660415ed73014d7c9706d45b5f969220c6
Eron Alves <eron.alves@rocketmail.com>
2026-05-07 14:51:13 -0300

    feat: add operation to repository

--- src/main/java/com/eronalves/nexuspay/wallet/WalletRepository.java
+++ src/main/java/com/eronalves/nexuspay/wallet/WalletRepository.java
@@ -1,9 +1,11 @@
 package com.eronalves.nexuspay.wallet;
 
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 
 public interface WalletRepository extends CrudRepository<Wallet, UUID> {
 
+  Optional<Wallet> findByNameAndUserId(String name, UUID userId);
 
 }

a16d65a55f7d24882586cfba3aec02adc5ea374d
Eron Alves <eron.alves@rocketmail.com>
2026-05-07 15:34:35 -0300

    refactor: rename

--- src/main/java/com/eronalves/nexuspay/infra/BaseController.java
+++ src/main/java/com/eronalves/nexuspay/infra/BaseController.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 public class BaseController {
 
-  protected ResponseEntity<Void> returnCreatedSensible(SensibleEntity entity) {
+  protected ResponseEntity<Void> createdSensible(SensibleEntity entity) {
     return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
         .buildAndExpand(entity.getId()).toUri()).body(null);
   }

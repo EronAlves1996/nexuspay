ea0fd338ac72572639858798f438d5803cc4c960
Eron Alves <eron.alves@rocketmail.com>
2026-05-21 15:48:43 -0300

    feat: create account classification table

---
+++ src/main/resources/db/migration/V5__create_account_classification_table.sql
@@ -0,0 +1,10 @@
+CREATE TYPE balance_side_enum enum ('PASSIVE', 'ACTIVE');
+create table account_classification (
+  id bigserial primary key,
+  balance_side balance_side_enum not null,
+  description varchar(255)
+);
+INSERT INTO account_classification (balance_side, description) VALUES 
+(/* 1 */'ACTIVE', 'Demand Deposits'),
+(/* 2 */'ACTIVE', 'Pending Transfers'),
+(/* 3 */'PASSIVE', 'User Deposits');
